a1cbf65afd87b2d8f1104435161c715be9c2e3b1
Dongfu Jiang <dongfu.jdf@gmail.com>
2025-08-11 03:03:27 -0400

    update pixel reaoner vision process to removemax ratio

--- verl_tool/llm_agent/vision_utils.py
+++ verl_tool/llm_agent/vision_utils.py
@@ -1,9 +1,55 @@
 import io
 import base64
 import numpy as np
+import torch
+from io import BytesIO
 from typing import Union, Optional
 from PIL import Image
-from verl.utils.dataset.vision_utils import process_image, process_video
+from .vision_process import fetch_image, fetch_video
+from verl.utils.dataset.vision_utils import VIDEO_FORMAT_HELP
+
+
+def process_image(image: dict | Image.Image) -> Image.Image:
+    if isinstance(image, Image.Image):
+        return image.convert("RGB")
+
+    if "bytes" in image:
+        assert "image" not in image, "Cannot have both `bytes` and `image`"
+        image["image"] = BytesIO(image["bytes"])
+
+    return fetch_image(image)
+
+def process_video(
+    video: dict,
+    nframes: Optional[int] = None,
+    fps: Optional[float] = None,
+    fps_min_frames: Optional[int] = None,
+    fps_max_frames: Optional[int] = None,
+) -> torch.Tensor:
+    """Converts a video dict into a [n_frames, 3, H, W] tensor
+
+    Add video sample FPS in a future MR
+    """
+
+    if not isinstance(video, dict) or "video" not in video:
+        raise NotImplementedError(VIDEO_FORMAT_HELP)
+    assert nframes is None or fps is None, "Can't use both `nframes` or `fps`"
+
+    # Shallow copy... since we might want to add some keys
+    video = dict(video)
+
+    contains_sampling_rules = "nframes" in video or "fps" in video
+    if not contains_sampling_rules:
+        if nframes is not None:
+            video["nframes"] = nframes
+        elif fps is not None:
+            video["fps"] = fps
+            if fps_min_frames is not None:
+                video["min_frames"] = fps_min_frames
+            if fps_max_frames is not None:
+                video["max_frames"] = fps_max_frames
+
+    return fetch_video(video)
 
 def encode_image(img: Image.Image) -> str:
     if isinstance(img, Image.Image):
