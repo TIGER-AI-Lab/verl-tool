7e309266434c15b48459a65ada9b51285c5544dd
dongfujiang <dongfu.jdf@gmail.com>
2025-08-26 01:03:37 +0800

    change deepsearch to use ray tool manager to avoid weird deadlock

--- examples/train/deepsearch/README.md
+++ examples/train/deepsearch/README.md
@@ -13,9 +13,9 @@ huggingface-cli download VerlTool/deepsearch google_search_cache.jsonl --local-d
 # Start the tool server
 host=localhost
 port=5000
-tool_type=google_search # separate by comma if you want to start multiple tool servers
+tool_type=google_search,python_code # separate by comma if you want to start multiple tool servers
 workers_per_tool=4 # number of workers for the tool server, meaning how many threads will be used to handle a single tool request with multiple trajectories
-python -m verl_tool.servers.serve --host $host --port $port --tool_type $tool_type --workers_per_tool $workers_per_tool & # run in background
+python -m verl_tool.servers.serve --host $host --port $port --tool_type $tool_type --workers_per_tool $workers_per_tool --use_ray True & # run in background
 ```
 ```bash
 python -m verl_tool.servers.tests.test_bing_search_tool bing_search --url=http://localhost:5000/get_observation
--- examples/train/deepsearch/eval.sh
+++ examples/train/deepsearch/eval.sh
@@ -1,14 +1,14 @@
 set -x
 dataset_name=deepsearch # or math_torl_offical to use torl training data
 train_data=$(pwd)/data/${dataset_name}/hard_search_1k.parquet
-val_data=[$(pwd)/data/${dataset_name}/gaia_test.parquet]
+val_data=[$(pwd)/data/${dataset_name}/hle_test.parquet]
 # val_data=[$(pwd)/data/${dataset_name}/gaia_test.parquet,\
 # $(pwd)/data/${dataset_name}/hle_test.parquet]
 # val_data=[$(pwd)/data/${dataset_name}/gaia_test.parquet,\
 # $(pwd)/data/${dataset_name}/hle_test.parquet,\
 # $(pwd)/data/${dataset_name}/webwalker_test.parquet,\
 # $(pwd)/data/${dataset_name}/xbench_test.parquet]
-model_name=VerlTool/deepsearch-qwen_qwen3-8b-grpo-n16-b128-t1.0-lr1e-6-new_global_step_70
+model_name=models/ds8b
 rl_alg=grpo # gae(ppo) or grpo, if grpo, then better set n>1 otherwise the group norm can not be effective
 n_gpus_per_node=4
 n_nodes=1
@@ -45,7 +45,8 @@ additional_eos_token_ids=[151645] # <|im_end|> token id
 mask_observations=True # mask observations for kl loss and gradient descent
 enable_mtrl=False # enable multi-turn training
 model_pretty_name=$(echo $model_name | tr '/' '_' | tr '[:upper:]' '[:lower:]')
-run_name_postfix="-qwen3-8b-summ"
+# run_name_postfix="-qwen3-8b-summ"
+run_name_postfix="-snippet-only"
 if [ "$enable_agent" = "True" ]; then
     run_name="${reward_manager}-${strategy}-agent-${model_pretty_name}-${rl_alg}-n${n}-b${batch_size}-t${temperature}-lr${lr}${run_name_postfix}"
 else
@@ -65,7 +66,7 @@ echo "action_stop_tokens_file=$action_stop_tokens_file"
 host=$(hostname -i | awk '{print $1}')
 port=$(shuf -i 30000-31000 -n 1)
 tool_server_url=http://$host:$port/get_observation
-python -m verl_tool.servers.serve --host $host --port $port --tool_type "google_search,python_code" --workers_per_tool 4 &
+python -m verl_tool.servers.serve --host $host --port $port --tool_type "google_search,python_code" --workers_per_tool 4 --use_ray True > logs/tool_server_$run_name.log 2>&1 &
 server_pid=$!
 
 echo "Server (pid=$server_pid) started at $tool_server_url"
