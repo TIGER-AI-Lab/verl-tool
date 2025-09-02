fbaf17222cc4866c59cf9c4d6dd3b3c36603faaa
Wyett <wyettzeng@gmail.com>
2025-09-02 00:21:28 -0400

    minor updates

--- acecoder_v2/request_gpu.sh
+++ acecoder_v2/request_gpu.sh
@@ -1,5 +1,5 @@
 # for actual training
-srun --account=aip-wenhu --gres=gpu:h100:8 --time=48:00:00 --job-name=wyett_acecoder_v2 -c 16 --mem=300GB --pty bash
+srun --account=aip-wenhu --gres=gpu:h100:8 --time=48:00:00 --job-name=wyett_acecoder_v2 -c 16 --mem=500GB --pty bash
 
 # for testing
 srun --account=aip-wenhu --gres=gpu:l40s:4 --time=12:00:00 --job-name=wyett_acecoder_v2_test -c 8 --mem=300GB --pty bash
