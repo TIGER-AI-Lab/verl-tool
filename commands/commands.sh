3646dd1d47d24aaa11832bc96063ed9375b13000
Dongfu Jiang <dongfu.jdf@gmail.com>
2025-11-17 18:17:55 -0800

    update

--- commands/commands.sh
+++
@@ -1,4 +0,0 @@
-bash commands/submit_train.sh examples/train/ablation/scaling_turns/math_tir_7b_grpo_1_turns.sh 1 2
-bash commands/submit_train.sh examples/train/ablation/scaling_turns/math_tir_7b_grpo_5_turns.sh 1 2
-bash commands/submit_train.sh examples/train/ablation/scaling_turns/math_tir_7b_grpo_5_turns_mtrl.sh 1 2
-bash commands/submit_train.sh examples/train/ablation/scaling_turns/math_tir_7b_grpo_10_turns.sh 1 2
