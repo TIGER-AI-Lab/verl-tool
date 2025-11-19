4b2e64680edb8306615e159f13f897edf2c1670c
Dongfu Jiang <dongfu.jdf@gmail.com>
2025-11-18 18:23:39 -0800

    remove files

--- commands/commands.sh
+++
@@ -1,4 +0,0 @@
-bash commands/submit_train.sh examples/train/acetoolreason/train_multi_node.sh 8 1
-bash commands/submit_train.sh examples/train/acetoolreason/train_multi_node_math_rl_no_tool.sh 2 1
-
-bash commands/submit_train.sh examples/train/acetoolreason/train_multi_node.sh 4 1
